5503c91b5ff639110009f65e79f16c3b5c1a44aa
Gabi Boros <gboros@servoy.com>
2018-01-04 17:07:34 +0200

    SVY-11998 daynavigator month display

--- daynavigator/daynavigator.js
+++ daynavigator/daynavigator.js
@@ -10,11 +10,13 @@ function($log, $timeout) {
         },
         link: function($scope, $element, $attrs) {
             var datepaginatorElement = null;
+            var selectedMonth = '';
             $timeout(function() {
-                datepaginatorElement = $element.children().first();
+                datepaginatorElement = $element.find('.svy-extra-daynavigator');
                 datepaginatorElement.datepaginator({
                     showCalendar: false,
                     onSelectedDateChanged: function(event, date) {
+                        selectedMonth = date.format('MMM')
                         if($scope.handlers.onChange) {
                             $scope.handlers.onChange(date);
                         }
@@ -30,10 +32,16 @@ function($log, $timeout) {
             });
 
             $scope.$watch("model.selectedDate", function(newValue, oldValue) {
+                var momentDate = selectedDateToMomentDate();
+                selectedMonth = momentDate.format('MMM');
                 if(datepaginatorElement) {
-                    datepaginatorElement.datepaginator('setSelectedDate', selectedDateToMomentDate());
+                    datepaginatorElement.datepaginator('setSelectedDate', momentDate);
                 }
             });
+
+            $scope.getMonth = function() {
+                return selectedMonth;
+            }
 
             function selectedDateToMomentDate() {
                 return $scope.model.selectedDate ? moment($scope.model.selectedDate) : moment();
