74245f557e642063216f1cd98f2127fefec9aba4
Paolo <paronne@servoy.com>
2018-01-11 11:08:03 +0200

    Enhanche timeline with a divider (group item by day)

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -1,79 +1,112 @@
 angular.module('servoyextraTimeline', ['servoy']).directive('servoyextraTimeline', ['$log', '$sce',
-function($log, $sce) {
-    return {
-        restrict: 'E',
-        scope: {
-            model: '=svyModel',
-            handlers: '=svyHandlers',
-            api: '=svyApi',
-            svyServoyapi: '='
-        },
-        controller: function($scope, $element, $attrs) {
-            if($scope.svyServoyapi.isInDesigner()) {
-                $scope.model.data = [{time: ' today', subject: 'timeline subject', content: 'timeline content' }];
-            }
+	function($log, $sce) {
+		return {
+			restrict: 'E',
+			scope: {
+				model: '=svyModel',
+				handlers: '=svyHandlers',
+				api: '=svyApi',
+				svyServoyapi: '='
+			},
+			controller: function($scope, $element, $attrs) {
+				if ($scope.svyServoyapi.isInDesigner()) {
+					$scope.model.data = [{ time: ' today', subject: 'timeline subject', content: 'timeline content' }];
+				}
 
-            var entryStyleClassFunc = null;
-            $scope.$watch("model.entryStyleClassFunc", function(newValue, oldValue) {
-                if($scope.model.entryStyleClassFunc) {
-                    entryStyleClassFunc = eval($scope.model.entryStyleClassFunc);
-                }
-            });
+				var entryStyleClassFunc = null;
+				$scope.$watch("model.entryStyleClassFunc", function(newValue, oldValue) {
+						if ($scope.model.entryStyleClassFunc) {
+							entryStyleClassFunc = eval($scope.model.entryStyleClassFunc);
+						}
+					});
 
-            $scope.getEntryStyleClass = function(entry) {
-                if(entryStyleClassFunc) {
-                    return entryStyleClassFunc(entry);
-                }
-                return '';
-            }
+				$scope.getEntryStyleClass = function(entry) {
+					if (entryStyleClassFunc) {
+						return entryStyleClassFunc(entry);
+					}
+					return '';
+				}
 
-            var entryRendererFunc = function(entry) {
-                return '<div class="feed-icon"></div>' +
-                '<div class="feed-subject">' + entry.subject + ' </div>' +
-                '<div class="feed-content">' + entry.content + '</div>' +
-                '<div class="feed-actions"><div class="pull-right"><i class="fa fa-clock-o"></i>' + entry.time + '</div></div>';
-            }
-            $scope.$watch("model.entryRendererFunc", function(newValue, oldValue) {
-                if($scope.model.entryRendererFunc) {
-                    entryRendererFunc = eval($scope.model.entryRendererFunc);
-                }
-            });
+//				var entryRendererFunc = function(entry) {
+//					return '<div class="feed-icon"></div>' + '<div class="feed-subject">' + entry.subject + ' </div>' + '<div class="feed-content">' + entry.content + '</div>' + '<div class="feed-actions"><div class="pull-right"><i class="fa fa-clock-o"></i>' + entry.time + '</div></div>';
+//				}
 
-            $scope.getEntryRenderer = function(entry) {
-                if(entryRendererFunc) {
-                    return $sce.trustAsHtml(entryRendererFunc(entry));
-                }
-                return '';
-            }
+				var entryRendererFunc = function(entry) {
+					var template;
+					if (entry.data && entry.data.isDivider) {
+						template = '<div class="label-header3 feed-divider">DEADLINE ' + entry.subject + '</div>';
+					} else {
+						template = '<div class="feed-icon"></div>' + '<i class="pull-right md md-check icon-round text-success"></i>' + '<div class="feed-subject">' + entry.subject + ' <i class="md md-note" ng-attr-title="{{model.entryRendererFunc}}"></i> </div>' + '<div class="feed-content">' + entry.content + '</div>';
+					}
+					return template;
+				}
+				
+				$scope.$watch("model.entryRendererFunc", function(newValue, oldValue) {
+						if ($scope.model.entryRendererFunc) {
+							entryRendererFunc = eval($scope.model.entryRendererFunc);
+						}
+					});
 
-            $scope.onEntryClick = function(entry, e) {
-                if($scope.handlers.onClick) {
-                    $scope.handlers.onClick(entry, e.target.id);
-                }
-            }
+				$scope.getEntryRenderer = function(entry) {
+					if (entryRendererFunc) {
+						return $sce.trustAsHtml(entryRendererFunc(entry));
+					}
+					return '';
+				}
 
-            $scope.$watch('model.foundset', function(oldValue, newValue) {
-                if(!$scope.svyServoyapi || $scope.svyServoyapi.isInDesigner()) return;
-                loadDataFromFoundset();
-            });
+				$scope.onEntryClick = function(entry, e) {
+					if ($scope.handlers.onClick) {
+						$scope.handlers.onClick(entry, e.target.id);
+					}
+				}
 
-            function loadDataFromFoundset() {
-                if($scope.model.foundset && $scope.model.foundset.viewPort.size) {
-                    $scope.model.data = []
-                    for(var i = 0; i < $scope.model.foundset.viewPort.size; i++) {
-                    	var row = $scope.model.foundset.viewPort.rows[i];
-                        var entry = {};
-                        entry['time'] = row['time'] != undefined ?row['time'] : '';
-                        entry['subject'] = row['subject'] != undefined ?row['subject'] : '';
-                        entry['content'] = row['content'] != undefined ?row['content'] : '';
-                        entry['tooltip'] = row['tooltip'] != undefined ?row['tooltip'] : '';
-                        entry['data'] = row['data'] != undefined ?row['data'] : '';
-                        $scope.model.data.push(entry);
-                    }
-                }
+				$scope.$watch('model.foundset', function(oldValue, newValue) {
+						if (!$scope.svyServoyapi || $scope.svyServoyapi.isInDesigner()) return;
+						loadDataFromFoundset();
+					});
 
-            }
-        },
-        templateUrl: 'servoyextra/timeline/timeline.html'
-    }
-}]);
+				var monthNames = ["January", "February", "March", "April", "May", "June",
+					"July", "August", "September", "October", "November", "December"];
+
+				function loadDataFromFoundset() {
+					if ($scope.model.foundset && $scope.model.foundset.viewPort.size) {
+						$scope.model.data = [];
+
+						var lastDate;
+						for (var i = 0; i < $scope.model.foundset.viewPort.size; i++) {
+							var row = $scope.model.foundset.viewPort.rows[i];
+
+							var entry = { };
+							if (row.time) {
+								var time = new Date(row.time);
+								// TODO allow date format here
+
+								if (!lastDate || lastDate.toDateString() != time.toDateString()) {
+									var entryDateDivider = new Object()
+									entryDateDivider.time = time.toDateString();
+									entryDateDivider.subject = time.getDate() + ' ' + monthNames[time.getMonth()];
+									entryDateDivider.data = {
+										isDivider: true
+									};
+									$scope.model.data.push(entryDateDivider);
+								}
+								
+								lastDate = time;
+								entry['time'] = time.toDateString();
+
+							}
+
+							//  entry['time'] = row['time'] != undefined ? row['time'] : '';
+							entry['subject'] = row['subject'] != undefined ? row['subject'] : '';
+							entry['content'] = row['content'] != undefined ? row['content'] : '';
+							entry['tooltip'] = row['tooltip'] != undefined ? row['tooltip'] : '';
+							entry['data'] = row['data'] != undefined ? row['data'] : '';
+							$scope.model.data.push(entry);
+						}
+					}
+
+				}
+			},
+			templateUrl: 'servoyextra/timeline/timeline.html'
+		}
+	}]);
