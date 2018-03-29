09671bfe9401ec8fe25c377dea3e308efb0e6c4b
Paolo <paronne@servoy.com>
2018-03-29 15:11:33 +0200

    Adjust size in anchored forms

--- lightboxgallery/lightboxgallery.js
+++ lightboxgallery/lightboxgallery.js
@@ -1,47 +1,46 @@
-angular.module('servoyextraLightboxgallery', ['servoy'])
-	.directive('servoyextraLightboxgallery', ['$timeout', '$window', function($timeout, $window) {  
-	    return {
-	      restrict: 'E',
-	      scope: {
-	    	  model: '=svyModel',
-			  api: "=svyApi"
-	      },
-	      link: function($scope, $element, $attrs) {
-		    	$scope.images = [];
-		    	  
+angular.module('servoyextraLightboxgallery', ['servoy']).directive('servoyextraLightboxgallery', ['$timeout', '$window', function($timeout, $window) {
+		return {
+			restrict: 'E',
+			scope: {
+				model: '=svyModel',
+				api: "=svyApi"
+			},
+			link: function($scope, $element, $attrs) {
+				$scope.images = [];
+
 				//create images on %scope.images
 				function createImagesFromFs() {
 					var images = [];
 					for (var i = 0; i < $scope.model.imagesFoundset.viewPort.rows.length; i++) {
 						/** @type {{image: {url: String}, thumbnail: {url: String}, caption: String}} */
 						var row = $scope.model.imagesFoundset.viewPort.rows[i];
-						var image = { 
-							url: row.image ? row.image.url.split('?')[0] : null, 
-							thumbUrl: row.thumbnail ? row.thumbnail.url.split('?')[0] : null, 
-							caption: row.caption ? row.caption : null 
+						var image = {
+							url: row.image && row.image.url ? row.image.url.split('?')[0] : null,
+							thumbUrl: row.thumbnail && row.thumbnail.url ? row.thumbnail.url.split('?')[0] : null,
+							caption: row.caption ? row.caption : null
 						}
 						if (!image.url) continue;
 						images.push(image);
 					}
 					$scope.images = images;
 					$timeout(function() {
-						$window.lightbox.option({
-						      'albumLabel': $scope.model.albumLabel,
-						      'fitImagesInViewpoer': $scope.model.fitImagesInViewpoer,
-						      'fadeDuration': $scope.model.fadeDuration,
-						      'fitImagesInViewport': $scope.model.fitImagesInViewport,
-						      'imageFadeDuration': $scope.model.imageFadeDuration,
-						      'positionFromTop': $scope.model.positionFromTop,
-						      'resizeDuration': $scope.model.resizeDuration,
-						      'wrapAround': $scope.model.wrapAround,
-						      'showImageNumberLabel': $scope.model.showImageNumberLabel
-					    })
-						$window.lightbox.init();
-					}, 0);
+							$window.lightbox.option({
+								'albumLabel': $scope.model.albumLabel,
+								'fitImagesInViewpoer': $scope.model.fitImagesInViewpoer,
+								'fadeDuration': $scope.model.fadeDuration,
+								'fitImagesInViewport': $scope.model.fitImagesInViewport,
+								'imageFadeDuration': $scope.model.imageFadeDuration,
+								'positionFromTop': $scope.model.positionFromTop,
+								'resizeDuration': $scope.model.resizeDuration,
+								'wrapAround': $scope.model.wrapAround,
+								'showImageNumberLabel': $scope.model.showImageNumberLabel
+							})
+							$window.lightbox.init();
+						}, 0);
 				}
-				
+
 				createImagesFromFs();
-				
+
 				//add a listener to get notified of changes of dataproviders or added and deleted records
 				$scope.model.imagesFoundset.addChangeListener(function(changes) {
 					if (!changes.viewportRowsUpdated && changes.viewPortSizeChanged) {
@@ -50,28 +49,34 @@ angular.module('servoyextraLightboxgallery', ['servoy'])
 						createImagesFromFs();
 					}
 				});
-				
+
 				$scope.getStyle = function() {
-					var style = {};
+					var style = { };
 					if ($scope.model.maxImageWidth) {
-						style.maxWidth = $scope.model.maxImageWidth + 'px';
+						if ($scope.model.maxImageWidth == -1) {
+							style.maxWidth = 'none';
+						} else {
+							style.maxWidth = $scope.model.maxImageWidth + 'px';
+						}
 					}
 					if ($scope.model.maxImageHeight) {
-						style.maxHeight = $scope.model.maxImageHeight + 'px';
+						if ($scope.model.maxImageHeight == -1) {
+							style.height = 'auto';
+						} else {
+							style.maxHeight = $scope.model.maxImageHeight + 'px';
+						}
 					}
 					return style;
 				}
-				
+
 				$scope.api.showLightbox = function() {
 					var firstImg = angular.element($element[0].querySelector('a'));
 					if (firstImg) {
 						$window.lightbox.start(firstImg);
 					}
 				}
-	      },
-	      controller: function($scope, $element, $attrs) {
-	    	 
-	      },
-	      templateUrl: 'servoyextra/lightboxgallery/lightboxgallery.html'
-	    };
-	  }])
+			},
+			controller: function($scope, $element, $attrs) { },
+			templateUrl: 'servoyextra/lightboxgallery/lightboxgallery.html'
+		};
+	}])
