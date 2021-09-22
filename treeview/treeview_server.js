d72725009abd714c644ca290436a99a2db9322af
Paolo <paronne@servoy.com>
2021-09-22 13:03:04 +0200

    SVYX-327 Add JSDoc example to render the TreeView as a table

--- treeview/treeview_server.js
+++ treeview/treeview_server.js
@@ -11,6 +11,19 @@
  *	treeviewDataSet.addRow([5,		3,		'George',		'media:///user.png']);
  *
  *	%%prefix%%%%elementName%%.setDataSet(treeviewDataSet);
+ *
+ *  // Render tree as a table by providing any additional 'column*' in the DataSet; additional columns must be prefixed with 'column'; e.g. columnone, columntwo...
+ *	var tableTreeviewDataSet = databaseManager.createEmptyDataSet( 0, ['id', 'pid', 'treeColumn', 'icon', 'column1', 'column2']);
+ *
+ *	tableTreeviewDataSet.addRow([null, null, 'Group', null, 'Description', 'Extra info']);	// header
+ *	tableTreeviewDataSet.addRow([1, null, 'Main group', 'media:///group.png', 'This is my the Main Group', 'Has (0) child']);
+ *	tableTreeviewDataSet.addRow([2, null, 'Second group', 'media:///group.png', 'This is my the Second Group', 'Has (1) child']);
+ *	tableTreeviewDataSet.addRow([3, 2, 'Subgroup', 'media:///group.png', 'This is my the Subgroup Group', 'Has (2) children']);
+ *	tableTreeviewDataSet.addRow([4, 3, 'Mark', 'media:///user.png', 'This is leaf Mark', 'Has (0) child']);
+ *	tableTreeviewDataSet.addRow([5, 3, 'George', 'media:///user.png', 'This is leaf George', 'Has (0) child']);
+ *
+ *	%%prefix%%%%elementName%%.setDataSet(tableTreeviewDataSet);
+ *
  */
 $scope.api.setDataSet = function(jsDataSet) {
 	$scope.model.jsDataSet = jsDataSet;
