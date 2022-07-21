55dbec400765b498e5e2717de91f71840228c6b3
Johan Compagner <jcompagner@servoy.com>
2022-07-21 10:47:55 +0200

    TinyMCE 6.0 doesn't support anymore tabfocus en the elements

--- projects/servoyextracomponents/src/htmlarea/htmlarea.ts
+++ projects/servoyextracomponents/src/htmlarea/htmlarea.ts
@@ -39,8 +39,6 @@ export class ServoyExtraHtmlarea extends ServoyBaseComponent<HTMLDivElement> {
         menubar: false,
         statusbar: false,
         readonly: false,
-        plugins: 'tabfocus',
-        tabfocus_elements: ':prev,:next',
         toolbar: 'fontselect fontsizeselect | bold italic underline | superscript subscript | undo redo |alignleft aligncenter alignright alignjustify | styleselect | outdent indent bullist numlist'
     };
     editor: Editor;
