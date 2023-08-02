421a656d2a7d2c5940f9b246890f38b8dc120f19
Gabi Boros <gboros@servoy.com>
2023-08-02 16:17:12 +0300

    SVY-18440 Bug with DbTreeview selection

--- projects/servoyextracomponents/src/dbtreeview/dbtreeview.ts
+++ projects/servoyextracomponents/src/dbtreeview/dbtreeview.ts
@@ -355,6 +355,7 @@ export class ServoyExtraDbtreeview extends ServoyBaseComponent<HTMLDivElement> i
             const node = this.tree.treeModel.getActiveNode();
             if (node) {
                 node.setIsActive(false);
+                node.blur();
             }
         }
     }
