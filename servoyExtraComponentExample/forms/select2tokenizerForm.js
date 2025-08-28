f559d93255c6c1da8a7acae8d148049a60977986
dtimut <55229042+dtimut@users.noreply.github.com>
2025-08-28 16:19:50 +0300

    SVY-20350 create a servoy-extra sample servoy application in the servoy-extra repo select 2 tokenizer

--- servoyExtraComponentExample/forms/select2tokenizerForm.js
+++ servoyExtraComponentExample/forms/select2tokenizerForm.js
@@ -1,6 +1,55 @@
 /**
  * @type {String}
  *
+ * @properties={typeid:35,uuid:"959F92C8-4C96-479A-ADF1-B67B022BE239"}
+ */
+var styleClassDP = null;
+
+/**
+ * @type {Number}
+ *
+ * @properties={typeid:35,uuid:"2F75857F-8BCE-4917-8085-53AF76CB29CA",variableType:8}
+ */
+var maximumSelectionSizeDP = 0;
+
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"145EF176-4BEA-46C0-B47E-FCAF81B0C619"}
+ */
+var valueSeparatorDP = ',';
+
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"D74231BF-6BA6-4BD9-8D55-DAFE3A583237"}
+ */
+var tooltipTextDP = 'Tooltip for select 2';
+
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"709EBC69-9BE0-4F81-A09D-54C1B772BB5A"}
+ */
+var searchingTextDP = 'Searching..........';
+
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"2EE87EF1-A4D4-4B81-A26B-162FED4F4C1F"}
+ */
+var placeholderTextDP = 'Placeholder text';
+
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"188E58B3-9181-4A9D-9A16-74D5F450E78B"}
+ */
+var noMatchesFoundTextDP = 'No matches found';
+
+/**
+ * @type {String}
+ *
  * @properties={typeid:35,uuid:"ACDE4FA6-A36C-4C20-86B7-F1A19F07FFF8"}
  */
 var selectDP = "This is the DP";
@@ -57,4 +106,146 @@ function onRequestFocusTrue(event) {
  */
 function onRequestFocusFalse(event) {
 	elements.select2tokenizer_1.requestFocus(false);
+}
+
+/**
+ * Click event. dataTarget parameter is used to identify inner html elements (by their data-target attribute).
+ *
+ * @param {JSEvent} event
+ * @param {String} dataTarget
+ *
+ * @properties={typeid:24,uuid:"093E05CB-C09F-4E97-9354-F9F6E83E08F5"}
+ */
+function onAction_allowNewEntries(event, dataTarget) {
+	elements.select2tokenizer_1.allowNewEntries = !elements.select2tokenizer_1.allowNewEntries;
+}
+
+/**
+ * Click event. dataTarget parameter is used to identify inner html elements (by their data-target attribute).
+ *
+ * @param {JSEvent} event
+ * @param {String} dataTarget
+ *
+ * @properties={typeid:24,uuid:"96EF432C-957D-4C87-A4D9-84754F35E023"}
+ */
+function onAction_closeOnSelect(event, dataTarget) {
+	elements.select2tokenizer_1.closeOnSelect = !elements.select2tokenizer_1.closeOnSelect;
+}
+
+/**
+ * Click event. dataTarget parameter is used to identify inner html elements (by their data-target attribute).
+ *
+ * @param {JSEvent} event
+ * @param {String} dataTarget
+ *
+ * @properties={typeid:24,uuid:"7C8C85FF-EE0E-4481-B07D-AB03DD8DC993"}
+ */
+function onAction_clearSearchTextOnSelect(event, dataTarget) {
+	elements.select2tokenizer_1.clearSearchTextOnSelect = !elements.select2tokenizer_1.clearSearchTextOnSelect;
+}
+
+/**
+ * Click event. dataTarget parameter is used to identify inner html elements (by their data-target attribute).
+ *
+ * @param {JSEvent} event
+ * @param {String} dataTarget
+ *
+ * @properties={typeid:24,uuid:"99EF469A-67BD-4E29-A6F8-2D6BED305F85"}
+ */
+function onAction_containSearchText(event, dataTarget) {
+	elements.select2tokenizer_1.containSearchText = !elements.select2tokenizer_1.containSearchText;
+}
+
+/**
+ * Click event. dataTarget parameter is used to identify inner html elements (by their data-target attribute).
+ *
+ * @param {JSEvent} event
+ * @param {String} dataTarget
+ *
+ * @properties={typeid:24,uuid:"47E0AFB9-46EE-45B8-ADD4-38C0F98BEB29"}
+ */
+function onAction_openOnUnselect(event, dataTarget) {
+	elements.select2tokenizer_1.openOnUnselect = !elements.select2tokenizer_1.openOnUnselect;
+}
+
+/**
+ * Click event. dataTarget parameter is used to identify inner html elements (by their data-target attribute).
+ *
+ * @param {JSEvent} event
+ * @param {String} dataTarget
+ *
+ * @properties={typeid:24,uuid:"2D6219E4-A091-4C8E-84E4-E616FE1CC997"}
+ */
+function onAction_overlayMode(event, dataTarget) {
+	elements.select2tokenizer_1.overlayMode = !elements.select2tokenizer_1.overlayMode;
+}
+
+/**
+ * Click event. dataTarget parameter is used to identify inner html elements (by their data-target attribute).
+ *
+ * @param {JSEvent} event
+ * @param {String} dataTarget
+ *
+ * @properties={typeid:24,uuid:"2BD29C29-7124-4193-AE4A-388054B6BD0B"}
+ */
+function onAction_editable(event, dataTarget) {
+	elements.select2tokenizer_1.editable = !elements.select2tokenizer_1.editable;
+}
+
+/**
+ * Click event. dataTarget parameter is used to identify inner html elements (by their data-target attribute).
+ *
+ * @param {JSEvent} event
+ * @param {String} dataTarget
+ *
+ * @properties={typeid:24,uuid:"288D69B3-0E23-4CD9-9710-0B2F802B2133"}
+ */
+function onAction_enabled(event, dataTarget) {
+	elements.select2tokenizer_1.enabled = !elements.select2tokenizer_1.enabled;
+}
+
+/**
+ * Click event. dataTarget parameter is used to identify inner html elements (by their data-target attribute).
+ *
+ * @param {JSEvent} event
+ * @param {String} dataTarget
+ *
+ * @properties={typeid:24,uuid:"28C08AD5-8CE4-4A81-84D1-7BC2D9C62B2A"}
+ */
+function onAction_visible(event, dataTarget) {
+	elements.select2tokenizer_1.visible = !elements.select2tokenizer_1.visible;
+}
+
+/**
+ * Handle changed data, return false if the value should not be accepted.
+ * JSEvent.data will contain extra information about dataproviderid, its scope and the scope id (record datasource or form/global variable scope)
+ *
+ * @param oldValue
+ * @param newValue
+ * @param {JSEvent} event
+ *
+ * @return {Boolean}
+ *
+ * @properties={typeid:24,uuid:"E9D07BAC-FD66-4369-BD7C-4E2DCBC54883"}
+ */
+function onDataChange_maximumSelectionSize(oldValue, newValue, event) {
+	elements.select2tokenizer_1.maximumSelectionSize = maximumSelectionSizeDP;
+	return true
+}
+
+/**
+ * Handle changed data, return false if the value should not be accepted.
+ * JSEvent.data will contain extra information about dataproviderid, its scope and the scope id (record datasource or form/global variable scope)
+ *
+ * @param oldValue
+ * @param newValue
+ * @param {JSEvent} event
+ *
+ * @return {Boolean}
+ *
+ * @properties={typeid:24,uuid:"8FA4A012-0BE2-4AFF-8D4D-F1F47DB04806"}
+ */
+function onDataChange_valueSeparator(oldValue, newValue, event) {
+	elements.select2tokenizer_1.valueSeparator = valueSeparatorDP;
+	return true
 }
