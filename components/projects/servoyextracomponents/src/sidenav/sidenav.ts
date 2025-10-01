b427b8961dfaf4a27f26a000c412d9411256fb8f
cPecican <cpecican@servoy.com>
2025-10-01 13:27:26 +0300

    SVY-20594 Sidenav with footerForm does not keep background color of footerForm when re-rendered

--- components/projects/servoyextracomponents/src/sidenav/sidenav.ts
+++ components/projects/servoyextracomponents/src/sidenav/sidenav.ts
@@ -151,6 +151,7 @@ export class ServoyExtraSidenav extends ServoyBaseComponent<HTMLDivElement> {
 								this.realFooterForm = this.footerForm;
 							}).finally(() => this.cdRef.detectChanges());
 						}
+                        this.addRemoveStickyStyle();
 						break;
 					case 'sidenavWidth':
 					case 'responsiveHeight':
